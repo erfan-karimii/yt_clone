a2c14d499ea9bd7499b30a48d4a34a2feee7ff7c
erfan <h410mi3@gmail.com>
2023-04-06 10:08:58 +0330

    fix category video count bug

--- core/youtube/templatetags/video_tags.py
+++ core/youtube/templatetags/video_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from datetime import datetime , timezone , timedelta
+from youtube.models import Category
 register = template.Library()
 
 
@@ -8,3 +9,9 @@ def history_video_days_passed(date):
     today = datetime.now(timezone.utc)
     days_passed = today - date
     return f"{days_passed.days} روز پیش" if days_passed.days != 0 else f'{timedelta(seconds=int(days_passed.seconds))} قبل '
+
+@register.simple_tag(name='count_category_video') 
+def count_category_video(cat_id):
+    category = Category.objects.get(id=cat_id)
+    video_count = category.video_set.filter(published=True).count()
+    return video_count
